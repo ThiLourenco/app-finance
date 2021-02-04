16867e77e15734e19a2fb6021064b99ea8e2cf35
Thigo <thiago.l17@yahoo.com>
2021-02-04 14:15:02 -0300

    added js

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-# Dev Finance
+<h1 align="center">
+  <img alt="dev.finances" title="dev.finances" src=".github/logo.svg" width="220px" />
+</h1>
 
-Aplicação de controle de gastos.
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
 
+
+<br>
+
+<p align="center">
+  <img alt="dev.finances" src="./github/logo.svg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+## 💻 Projeto
+
+O dev.finances é uma aplicação de controle financeiro, onde é possível cadastrar e excluir transações e ver o saldo de entrada e saída 💰
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [desse link](https://www.figma.com/file/7Vu9DzUaCZIV4nibzkjgB4/dev.finance%24-Maratona-Discover). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Projeto criado durante a maratona discovery da [Rocketseat](https://rocketseat.com.br/).
+
+Feito com ♥ by Thiago Lourenço
---
+++ scripts/main.js
@@ -0,0 +1,212 @@
+const btnCancel = document.querySelector('.cancel');
+const btnTransaction = document.querySelector('.new');
+const modal = document.querySelector('.modal-overlay');
+const form = document.querySelector('form');
+
+const addTransaction = () => {
+  modal.classList.add('active');
+}
+
+const cancelTransaction = () => {
+  modal.classList.remove('active');
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+    try {
+        Form.validateFields()
+        const transaction = Form.formatValues()
+        Transaction.add(transaction)
+        Form.clearFields()
+        cancelTransaction()
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+const getStorage = () => {
+  let data = '';
+  return data = JSON.parse(localStorage.getItem('dev.finances:transactions')) || [];
+}
+
+const setStorage = (transactions) => {
+  localStorage.setItem('dev.finances:transactions', JSON.stringify(transactions))
+}
+
+const Transaction = {
+  all: getStorage(),
+
+  add(transaction){
+      Transaction.all.push(transaction);
+
+      App.reload();
+  },
+
+  remove(index) {
+      Transaction.all.splice(index, 1);
+
+      App.reload();
+  },
+
+  incomes() {
+      let income = 0;
+      Transaction.all.forEach(transaction => {
+          if( transaction.amount > 0 ) {
+              income += transaction.amount;
+          }
+      })
+      return income;
+  },
+
+  expenses() {
+      let expense = 0;
+      Transaction.all.forEach(transaction => {
+          if( transaction.amount < 0 ) {
+              expense += transaction.amount;
+          }
+      })
+      return expense;
+  },
+
+  total() {
+      return Transaction.incomes() + Transaction.expenses();
+  }
+}
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+      const tr = document.createElement('tr')
+      tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+      tr.dataset.index = index;
+
+      DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction(transaction, index) {
+      const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+      const amount = Utils.formatCurrency(transaction.amount);
+
+      const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+          <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
+      </td>
+      `;
+
+      return html;
+  },
+
+  updateBalance() {
+      document
+          .getElementById('incomeDisplay')
+          .innerHTML = Utils.formatCurrency(Transaction.incomes());
+      document
+          .getElementById('expenseDisplay')
+          .innerHTML = Utils.formatCurrency(Transaction.expenses());
+      document
+          .getElementById('totalDisplay')
+          .innerHTML = Utils.formatCurrency(Transaction.total());
+  },
+
+  clearTransactions() {
+      DOM.transactionsContainer.innerHTML = "";
+  }
+}
+
+const Utils = {
+  formatAmount(value){
+      value = Number(value.replace(/\,\./g, "")) * 100;
+      
+      return value;
+  },
+
+  formatDate(date) {
+      const splittedDate = date.split("-");
+      return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
+
+  formatCurrency(value) {
+      const signal = Number(value) < 0 ? "-" : "";
+
+      value = String(value).replace(/\D/g, "");
+
+      value = Number(value) / 100;
+
+      value = value.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL"
+      });
+
+     return signal + value;
+  }
+}
+
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+      return {
+          description: Form.description.value,
+          amount: Form.amount.value,
+          date: Form.date.value
+      };
+  },
+
+  validateFields() {
+      const { description, amount, date } = Form.getValues();
+      
+      if( description.trim() === '' || 
+          amount.trim() === '' || 
+          date.trim() === '' ) {
+              throw new Error('Por favor, preencha todos os campos');
+      }
+  },
+
+  formatValues() {
+      let { description, amount, date } = Form.getValues();
+      
+      amount = Utils.formatAmount(amount);
+
+      date = Utils.formatDate(date);
+
+      return {
+          description,
+          amount,
+          date
+      };
+  },
+
+  clearFields() {
+      Form.description.value = '';
+      Form.amount.value = '';
+      Form.date.value = '';
+  },
+}
+
+const App = {
+  init() {
+      Transaction.all.forEach(DOM.addTransaction);
+      
+      DOM.updateBalance();
+
+      setStorage(Transaction.all);
+  },
+  reload() {
+      DOM.clearTransactions();
+      App.init();
+  },
+}
+
+btnTransaction.addEventListener('click', addTransaction);
+btnCancel.addEventListener('click', cancelTransaction);
+form.addEventListener('submit', handleSubmit);
+
+App.init();
